258e912b7c8513997c1be9bfd0c851e2f6ccd272
Craig Burke <c.burke@rae.com>
2026-02-04 12:16:48 -0500

    Bump version to 2.3.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ package_name = 'pipe_crawler_control'
 
 setup(
     name=package_name,
-    version='2.3.2',
+    version='2.3.3',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
